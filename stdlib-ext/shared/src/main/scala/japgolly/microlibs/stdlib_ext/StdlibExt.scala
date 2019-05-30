4c3f65e31d0a371f42bf8dda9abfe0ebae55be07
David Barri <japgolly@gmail.com>
2019-05-30 19:16:02 +1000

    Add JavaTimeHelpers

--- stdlib-ext/shared/src/main/scala/japgolly/microlibs/stdlib_ext/StdlibExt.scala
+++ stdlib-ext/shared/src/main/scala/japgolly/microlibs/stdlib_ext/StdlibExt.scala
@@ -2,6 +2,8 @@ package japgolly.microlibs.stdlib_ext
 
 import java.io.{PrintWriter, StringWriter}
 import java.time.Duration
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.FiniteDuration
 import scala.reflect.ClassTag
 import scala.util.matching.Regex
 
@@ -286,6 +288,46 @@ object StdlibExt {
     def minutes: Duration = (n * 60).seconds
     def hours  : Duration = (n * 60).minutes
     def days   : Duration = (n * 24).hours
+  }
+
+  @inline implicit class DurationExt(private val d: Duration) extends AnyVal {
+    def isShorterThan(x: Duration): Boolean =
+      d.compareTo(x) < 0
+
+    def isLongerThan(x: Duration): Boolean =
+      d.compareTo(x) > 0
+
+    def asFiniteDuration: FiniteDuration =
+      FiniteDuration(d.toNanos, TimeUnit.NANOSECONDS)
+
+    def asMillis: Double =
+      asSeconds * 1000.0
+
+    def asSeconds: Double =
+      d.getSeconds.toDouble + d.getNano.toDouble / 1000000000
+
+    def asMinutes: Double =
+      asSeconds / 60.0
+
+    def conciseDesc: String =
+      if (d.getSeconds == 0) {
+        val n = d.getNano
+        if      (n >= 1000000) (n/1000000) + " ms"
+        else if (n >= 1000)    (n/1000)    + " us"
+        else                   n           + " ns"
+      } else {
+        val sec = asSeconds
+        if (sec <= 60) "%.1f sec".format(sec) else {
+          val min = sec / 60.0
+          if (min <= 60) "%.1f min".format(min) else {
+            val hr = min / 60.0
+            if (hr <= 24) "%.2f hr".format(hr) else {
+              val days = hr / 24.0
+              "%.2f days".format(days)
+            }
+          }
+        }
+      }
   }
 
   @inline implicit class RegexExt(private val self: Regex) extends AnyVal {
--- stdlib-ext/shared/src/test/scala/japgolly/microlibs/stdlib_ext/StdlibExtTest.scala
+++ stdlib-ext/shared/src/test/scala/japgolly/microlibs/stdlib_ext/StdlibExtTest.scala
@@ -2,6 +2,7 @@ package japgolly.microlibs.stdlib_ext
 
 import utest._
 import StdlibExt._
+import java.time.Duration
 
 object StdlibExtTest extends TestSuite {
   override def tests = Tests {
@@ -45,6 +46,47 @@ object StdlibExtTest extends TestSuite {
       "x 1 a 2" - test("x 1 a 2")(1, 2)
       "x 1 a 2 y" - test("x 1 a 2 y")(1, 2)
       "xx 11 aa 22 yy" - test("xx 11 aa 22 yy")(1, 1, 2, 2)
+    }
+
+    'duration {
+      'toSeconds {
+        val d = Duration.ofMillis(1100) plus Duration.ofNanos(9002003L)
+        d.asSeconds ==> 1.109002003
+      }
+
+      'conciseDesc {
+        def test(sec: Double, ns: Long, expect: String): Unit = {
+          val d = Duration.ofSeconds(sec.toLong).plus(Duration.ofNanos(ns))
+          val a = d.conciseDesc
+          a ==> expect
+        }
+
+        'ns1 - test(0,   1,   "1 ns")
+        'ns2 - test(0,  11,  "11 ns")
+        'ns3 - test(0, 111, "111 ns")
+
+        'us1 - test(0,   1001,   "1 us")
+        'us2 - test(0,  11001,  "11 us")
+        'us3 - test(0, 111001, "111 us")
+
+        'ms1 - test(0,   1001001,   "1 ms")
+        'ms2 - test(0,  11001001,  "11 ms")
+        'ms3 - test(0, 111001001, "111 ms")
+
+        'sec1 - test( 0, 1001001001,  "1.0 sec")
+        'sec2 - test( 0, 1901001001,  "1.9 sec")
+        'sec3 - test(59,  901001001, "59.9 sec")
+
+        'min1 - test( 1.09 * 60, 3,  "1.1 min")
+        'min2 - test(12.94 * 60, 3, "12.9 min")
+        'min3 - test(59.88 * 60, 3, "59.9 min")
+
+        'hr1 - test(1.02 * 3600, 3, "1.02 hr")
+        'hr2 - test(23 * 3600, 3, "23.00 hr")
+
+        'day1 - test(36 * 3600, 3, "1.50 days")
+        'day2 - test(48 * 3600, 3, "2.00 days")
+      }
     }
   }
 }
