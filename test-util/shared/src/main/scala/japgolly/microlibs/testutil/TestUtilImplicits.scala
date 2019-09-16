484270ef5912de6a39bfce354e72acb80e90c9fa
David Barri <japgolly@gmail.com>
2019-09-17 09:49:22 +1000

    TestUtil: Add Either getOrThrow

---
+++ test-util/shared/src/main/scala/japgolly/microlibs/testutil/TestUtilImplicits.scala
@@ -0,0 +1,23 @@
+package japgolly.microlibs.testutil
+
+import japgolly.microlibs.testutil.TestUtil.fail
+import sourcecode.Line
+
+trait TestUtilImplicits {
+  import TestUtilImplicits._
+
+  implicit def toTestUtilEitherExt[A, B](x: Either[A, B])(implicit l: Line): EitherExt[A, B] =
+    new EitherExt(x)
+}
+
+object TestUtilImplicits {
+
+  implicit class EitherExt[A, B](private val self: Either[A, B])(implicit l: Line) {
+    def getOrThrow(): B =
+      self.fold(e => fail(s"Expected Right(_), found Left($e)", clearStackTrace = false), identity)
+
+    def getOrThrow(moreInfo: => String): B =
+      self.fold(e => fail(s"${moreInfo.replaceFirst("\\.?$", ".")} Expected Right(_), found Left($e)"), identity)
+  }
+
+}
