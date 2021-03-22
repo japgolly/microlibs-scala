6cd932f14376b5e0687121ac7d1356915e9eccf5
David Barri <japgolly@gmail.com>
2021-03-22 11:36:00 +1100

    Let adtValuesManually diverge between Scala 2 & 3

--- adt-macros/shared/src/test/scala/japgolly/microlibs/adt_macros/AdtMacroTest.scala
+++ adt-macros/shared/src/test/scala/japgolly/microlibs/adt_macros/AdtMacroTest.scala
@@ -44,29 +44,6 @@ object AdtMacroTest extends TestSuite {
       // }
     }
 
-    "adtValuesManually" - {
-      "simple" - {
-//      's1i - assertOrderedNEV(MonoS1.ValuesM)(MonoS1.A)
-//      's3i - assertOrderedNEV(MonoS3.ValuesM)(MonoS3.A, MonoS3.B, MonoS3.C)
-      "s1" - assertOrderedNEV(adtValuesManually[MonoS1]()(MonoS1.A))(MonoS1.A)
-      "s3" - assertOrderedNEV(adtValuesManually[MonoS3]()(MonoS3.A, MonoS3.B, MonoS3.C))(MonoS3.A, MonoS3.B, MonoS3.C)
-      }
-      "dupTypes" - {
-        "ok" - assertOrderedNEV(adtValuesManually[MonoD2](allowDuplicateTypes = true)(MonoD2.A, MonoD2.B(true), MonoD2.B(false)))(MonoD2.A, MonoD2.B(true), MonoD2.B(false))
-        "ko" - assertFail(compileError("adtValuesManually[MonoD2]()(MonoD2.A, MonoD2.B(true), MonoD2.B(false))"))
-      }
-      "dupValues" - {
-        "ok" - assertOrderedNEV(adtValuesManually[MonoS3](allowDuplicateValues = true)(MonoS3.A, MonoS3.B, MonoS3.C, MonoS3.B))(MonoS3.A, MonoS3.B, MonoS3.C, MonoS3.B)
-        "S1" - assertFail(compileError("adtValuesManually[MonoS1]()(MonoS1.A, MonoS1.A)"))
-        "S3" - assertFail(compileError("adtValuesManually[MonoS3]()(MonoS3.A, MonoS3.B, MonoS3.B, MonoS3.C)"))
-        "dupD2" - assertFail(compileError("adtValuesManually[MonoD2](allowDuplicateTypes = true)(MonoD2.B(true), MonoD2.A, MonoD2.B(true), MonoD2.B(false))"))
-      }
-      "incomplete" - {
-        "O" - assertFail(compileError("adtValuesManually[MonoS3]()(MonoS3.A, MonoS3.C)"))
-        "C" - assertFail(compileError("adtValuesManually[MonoD2]()(MonoD2.A)"))
-      }
-    }
-
     "valuesForAdt" - {
       "ok" - {
         import MonoD._
