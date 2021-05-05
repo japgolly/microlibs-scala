39a6659560cdd1538c6b241a9b7ee07d79267ca7
David Barri <japgolly@gmail.com>
2021-05-05 11:30:54 +1000

    Add {Inline,Quoting}Utils

---
+++ macro-utils/shared/src/main/scala-3/japgolly/microlibs/macro_utils/InlineUtils.scala
@@ -0,0 +1,24 @@
+package japgolly.microlibs.macro_utils
+
+object InlineUtils:
+
+  inline def warn(inline warning: String): Unit =
+    ${ QuotingUtils.warn('warning) }
+
+  transparent inline def replaceFirst(str: String, regex: String, repl: String): String =
+    ${ QuotingUtils.replaceFirst('str, 'regex, 'repl) }
+
+  transparent inline def replaceAll(str: String, regex: String, repl: String): String =
+    ${ QuotingUtils.replaceAll('str, 'regex, 'repl) }
+
+  transparent inline def trim(str: String): String =
+    ${ QuotingUtils.trim('str) }
+
+  transparent inline def toLowerCase(str: String): String =
+    ${ QuotingUtils.toLowerCase('str) }
+
+  transparent inline def toUpperCase(str: String): String =
+    ${ QuotingUtils.toUpperCase('str) }
+
+  transparent inline def toInt(str: String): Int =
+    ${ QuotingUtils.toInt('str) }
