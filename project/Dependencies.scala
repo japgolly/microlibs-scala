5cf12ae2684fa0697f9645f49ba097dcef38ef31
Scala Steward <me@scala-steward.org>
2021-05-28 12:23:17 +0200

    Update scala-compiler, scala-library to 2.12.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   object Ver {
 
     // Exported
-    val Scala212        = "2.12.13"
+    val Scala212        = "2.12.14"
     val Scala213        = "2.13.5"
     val Scala3          = "3.0.0-RC3"
     val ScalaCollCompat = "2.4.3"
