296eed07e70c5bd9736491c47357d5d0fe161ca6
David Barri <japgolly@gmail.com>
2021-06-03 11:33:23 +1000

    2.12 doesn't want to compile? Fuck off then

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,6 @@ object Dependencies {
   object Ver {
 
     // Exported
-    val scala212        = "2.12.13"
     val scala213        = "2.13.6"
     val scala3          = "3.0.0"
     val scalaCollCompat = "2.4.4"
