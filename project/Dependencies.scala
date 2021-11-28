eecdcadaacc9f420e8d34278ed5f3efa47017db1
Scala Steward <me@scala-steward.org>
2021-11-28 04:33:29 +0100

    Update cats-core, cats-free to 2.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   object Ver {
 
     // Exported
-    val cats            = "2.6.1"
+    val cats            = "2.7.0"
     val scala2          = "2.13.6"
     val scala3          = "3.0.2"
     val sourceCode      = "0.2.7"
