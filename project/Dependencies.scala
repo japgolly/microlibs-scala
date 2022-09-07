5c7976a6d71c184553565997c37525ebf59db813
Scala Steward <scala_steward@virtuslab.com>
2022-09-07 16:49:20 +0000

    Update nyaya-gen to 1.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     // Internal
     def jamm            = "0.3.3"
     def kindProjector   = "0.13.2"
-    def nyaya           = "1.0.0"
+    def nyaya           = "1.1.0"
     def scalaCheck      = "1.15.4"
     def scalaJsJavaTime = "2.4.0"
     def utest           = "0.8.0"
