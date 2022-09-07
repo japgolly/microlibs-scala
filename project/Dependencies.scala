1559c051cf424cbe0869f6cab307b53212478288
Scala Steward <scala_steward@virtuslab.com>
2022-09-07 16:49:35 +0000

    Update scala3-library, ... to 3.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     // Exported
     def cats            = "2.8.0"
     def scala2          = "2.13.8"
-    def scala3          = "3.1.3"
+    def scala3          = "3.2.0"
     def sourceCode      = "0.3.0"
     def univEq          = "2.0.0"
 
